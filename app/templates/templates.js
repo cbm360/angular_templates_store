edb2e3393c66aa6776bd007305a5b3a8e3360dbc
Camron Moore <camronbmoore@gmail.com>
2016-04-01 14:15:58 -0600

    Added thumbnail images to details view.

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -28,6 +28,10 @@ angular.module('templateStore.templates', ['ngRoute'])
       })[0];
       $scope.mainImage = $scope.template.images[0].name;
     });
+
+    $scope.setImage = function(image){
+      $scope.mainImage = image.name;
+    }
 }]);
 
 
