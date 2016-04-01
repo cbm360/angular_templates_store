7a5854ca3ef0c32c22d252b52089a82954cf1f3a
Camron Moore <camronbmoore@gmail.com>
2016-04-01 14:09:37 -0600

    Added json file for templates, and accessed over http.  Then looped through the records to create html display.

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -5,11 +5,29 @@ angular.module('templateStore.templates', ['ngRoute'])
     when('/templates', {
       templateUrl: 'templates/templates.html',
       controller: 'TemplatesCtrl'
+    }).
+    when('/templates/:templateId', {
+      templateUrl: 'templates/templates-details.html',
+      controller: 'TemplateDetailsCtrl'
+    })
+}])
+
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('json/templates.json')
+    .success(function(data){
+      $scope.templates = data;
     });
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope){
-  console.log($scope);
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+  var templateId = $routeParams.templateId;
+  $http.get('json/templates.json')
+    .success(function(data){
+      $scope.template = $filter('filter')(data, function(d){
+        return d.id == templateId;
+      })[0];
+      $scope.mainImage = $scope.template.images[0].name;
+    });
 }]);
 
 
